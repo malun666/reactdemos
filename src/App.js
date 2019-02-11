0f70cf5fa02b6b4de68a957fb0477a8f7075c09b
aicoder <malun666@126.com>
2019-02-11 12:15:09 +0800

    added the jsx demos

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 // 1. 简单数据绑定(jsx语法)
 // 2. 属性也可以进行绑定
 // 3. {}中可以执行任何的js代码：三元运算， 函数调用等...
+// 4. jsx防注入
+// 5. 怎么把一个数组直接显示在页面上
 
 const date = Date.now();
 const element = (
@@ -15,6 +17,14 @@ const element = (
 
 const pId = 'aicoder';
 const num = 7;
+const dElement = '<h3>sdsf</h3>';
+const arr = [
+  <div>这是数组中的第一个元素</div>,
+  <div>这是第二个元素！</div>,
+  '<span>这是字符串！！</span>'
+];
+
+const arrCities = [ '北京', '上海', '天津'];
 
 class App extends Component {
 
@@ -37,6 +47,28 @@ class App extends Component {
         <hr/>
         <p>{ this.getDay(333) }</p>
         <p> { num > 8 ? 'hi,aicoder' : 'null'}</p>
+        <hr/>
+        <div>
+          { dElement }
+        </div>
+        <hr/>
+        <div>
+          这是数组的绑定展示：
+          { arr }
+        </div>
+        <hr/>
+        <div>
+          所有的城市：
+          <ul>
+            {
+              arrCities.map(item => {
+                return (
+                  <li> { item } </li>
+                );
+              })
+            }
+          </ul>
+        </div>
       </div>
     );
   }
